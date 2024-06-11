220dfc5e0187b98fb172b2735c95d9715fa5bfeb
ilse mar <ilse1991@gmail.com>
2024-06-10 19:34:56 -0600

    clase-1

---
+++ variables.js
@@ -0,0 +1,33 @@
+// VARIABLES Y TIPOS DE DATOS PRIMITIVOS (STRINGS, NÚMEROS, BOOLEANOS)
+
+let nombre = "pepe";
+let apellido = "perez";
+
+// para mostrar si se aplica
+// alert(nombre);
+// alert(apellido);
+
+console.log(nombre);
+console.log(apellido);
+
+const documento = "12341234";
+
+// let numero = prompt ("ingresa un numero");
+// console.log(numero)
+
+// let nombreDeUsuario = prompt("inicia sesion con tu nombre");
+// alert("hola " + nombreDeUsuario + " como estás?");
+
+// let n1 = prompt("ingresa el primer numero");
+// let n2 = prompt("ingresa el segundo numero");
+
+let n1 = Number(prompt("ingresa el primer numero"));
+
+let n2 = Number(prompt("ingresa el segundo numero"));
+
+let total = n1 + n2;
+console.log(total);
+
+//DATOS BOOLEAN/BOOLEANOS (T o F)
+
+console.log(n1 > n2);
