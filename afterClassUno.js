8fdc0a77bd3ea09c731115dd4dcc3c37786dcaa8
ilse mar <ilse1991@gmail.com>
2024-06-23 18:50:27 -0600

    afterClassUno

---
+++ afterClassUno.js
@@ -0,0 +1,66 @@
+// saber si es mayor de edad
+
+// let edad = prompt("ingresa tu edad");
+// if (edad >= 18) {
+//   alert("es mayor de edad");
+// } else {
+//   alert("no es mayor de edad");
+// }
+
+// saber si es fin de semana (si es sábado o domingo)
+
+// let dia = prompt("qué dia de la semana es?");
+
+// if (dia === "sabado" || dia === "domingo" || dia === "sábado") {
+//   alert(`Hoy es ${dia}, por ello es fin de semana`);
+// } else if (
+//   dia === "lunes" ||
+//   dia === "martes" ||
+//   dia === "miercoles" ||
+//   dia === "miércoles" ||
+//   dia === "jueves" ||
+//   dia === "viernes"
+// ) {
+//   alert(`Hoy es ${dia}, por eso NO es fin de semana`);
+// } else {
+//   alert("Dato no válido");
+// }
+
+//saber si los siguientes 2 números son positivos o no
+//si ambos son positivos
+//si uno es positivo
+// si ninguno es positivo
+
+// let n1 = -5;
+// let n2 = 10;
+
+// if (n1 >= 0 && n2 >= 0) {
+//   alert("ambos son positivos");
+// } else if (n1 >= 0 || n2 >= 0) {
+//   if (n1 > 0) {
+//     alert("n1 es positivo");
+//   } else if (n2 > 0) {
+//     alert("n2 es positivo");
+//   } else {
+//     alert("dato incorrecto");
+//   }
+// } else {
+//   alert("ninguno es positivo");
+// }
+
+//Saber si un numero es par
+
+let n1 = 20;
+let n2 = 3;
+
+console.log(n1 % n2); //operador del modulo: residuo
+
+let numero = 6;
+// alert(numero % 2 === 0);
+
+numero % 2 === 0 ? alert("es par") : alert("NO es par");
+// if (numero % 2 === 0) {
+//   alert("es par");
+// } else {
+//   alert("NO es par");
+// }
